4a35d0475aeb85ca12856a831864165f9aa8436f
ulhas666 <shringareulhas09@gmail.com>
2012-05-23 19:52:41 +0530

    first commit

---
+++ adref.js
@@ -0,0 +1,8 @@
+var section=3893;var width=468;var height=60;var enc=1;var clicktag="http%3A//adnet.affinity.com/adlink%2F5359%2F2359613%2F0%2F1%2FAdId%3D2586164%3BBnId%3D6%3Bitime%3D779009972%3Bkey%3DComputers%20%26%20Electronics%2BOpen%20Source%2BWindows%20%26%20%2ENET%2BSoftware%2BScience%2BEngineering%20%26%20Technology%2BProgramming%3Blink%3D";var pop=0;
+document.write("<scr"+"ipt type=\"text/javascript\" src=\"http://cdn.atomex.net/static/js/ads-min.js\">\n");
+document.write("</scr"+"ipt>\n");
+document.write("<noscript><iframe src=\"http://ads.atomex.net/cgi-bin/adserver.fcgi/ad?section=3893&width=468&height=60&enc=1&type=iframe&js=0&clickTag=http://adnet.affinity.com/adlink/5359/2359613/0/1/AdId=2586164;BnId=6;itime=779009972;key=Computers%20%26%20Electronics+Open%20Source+Windows%20%26%20.NET+Software+Science+Engineering%20%26%20Technology+Programming;nodecode=yes;link=http%3A//adnet.affinity.com/adlink%2F5359%2F2359613%2F0%2F1%2FAdId%3D2586164%3BBnId%3D6%3Bitime%3D779009972%3Bkey%3DComputers%20%26%20Electronics%2BOpen%20Source%2BWindows%20%26%20%2ENET%2BSoftware%2BScience%2BEngineering%20%26%20Technology%2BProgramming%3Blink%3D\" height=\"60\" width=\"468\" scrolling=\"no\" marginwidth=\"0\" marginheight=\"0\" frameborder=\"0\" ></iframe></noscript>\n");
+
+var adcount_2359613_1_=new Image();
+adcount_2359613_1_.src="http://adnet.affinity.com/adcount/3.0/5359/2359613/0/1/AdId=2586164;BnId=6;ct=4072240632;st=8596;adcid=1;itime=779009972;reqtype=5;";
+
